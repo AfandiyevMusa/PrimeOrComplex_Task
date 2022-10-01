e66f1a03e3c75ed7163f54d1b25d7c93bd686298
Main <Main>
2022-10-01 19:55:07 +0400

    added the file that contain code to master branch

---
+++ PrimeOrComplex/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PrimeOrComplex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4, cnt = 0;
+            if (n > 1)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        cnt++;
+                    }
+                }
+                if (cnt > 2)
+                {
+                    Console.WriteLine("n is complex number!");
+                }
+                else
+                {
+                    Console.WriteLine("n is prime number");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please, write the correct number!");
+            }
+        }
+    }
+}
